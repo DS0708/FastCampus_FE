4aae8b4506b1ba57bee039425f45a2899f8ea34f
DS0708 <tlsehdtjd070@naver.com>
2023-12-21 18:54:54 +0900

    part1. js clear

--- Part1_HTML_CSS_JS/js_example/main.js
+++ Part1_HTML_CSS_JS/js_example/main.js
@@ -1,20 +1,42 @@
-const person1 = {
-  name : "ds",
-  getName : function () {
-    return this.name;
-  }
+// const person1 = {
+//   name : "ds",
+//   getName : function () {
+//     return this.name;
+//   }
+// }
+
+// const person2 = {
+//   name : "sh",
+//   gn : person1.getName
+// }
+
+// const person3 = {
+//   name : "SDS",
+//   gn : person1.getName()
+// }
+
+// console.log(person1.getName());
+// console.log(person2.gn());
+// console.log(person3.gn);
+
+const boxEL = document.querySelector('.box');
+
+function printActiveContains (){
+  console.log(boxEL.classList.contains('active'))
 }
 
-const person2 = {
-  name : "sh",
-  gn : person1.getName
-}
+boxEL.addEventListener('click',function () {
+  console.log('Click !!');
+  boxEL.classList.add('active');
+  printActiveContains();
+  boxEL.classList.remove('active');
+  printActiveContains();
+})
 
-const person3 = {
-  name : "SDS",
-  gn : person1.getName()
-}
+const itemELs = document.querySelectorAll('.item');
 
-console.log(person1.getName());
-console.log(person2.gn());
-console.log(person3.gn);
+itemELs.forEach(function(itemEl,index){
+  itemEl.classList.add(`order-${index+1}`);
+  console.log(index, itemEl.classList);
+  itemEl.textContent = itemEl.className;
+})
