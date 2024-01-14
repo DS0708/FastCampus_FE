fe7b3028ce88561ea077c277843c2a271c69d62d
DS0708 <tlsehdtjd070@naver.com>
2024-01-14 19:02:31 +0900

    CH04 앱 시간 여행 추가

--- Ch02_Create_TicTacToe_App/react-tictactoe-app/src/components/Board.js
+++ Ch02_Create_TicTacToe_App/react-tictactoe-app/src/components/Board.js
@@ -1,54 +1,8 @@
-import React, { useState } from 'react'
 import Square from './Square'
 import "./Board.css"
 
-const Board = () => {
+const Board = ({squares, handleClick}) => {
 
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6], 
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-
-    return null;
-
-  }
-  const winner = calculateWinner(squares);
-  
-  let status;
-  if (winner) {
-    status = `Winner: ${winner}`;
-  }else {
-    status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
-  }
-
-  const handleClick =(i) => {
-    const newSquares = squares.slice();
-    
-    if (calculateWinner(newSquares) || newSquares[i]) {
-      return;
-    }
-
-    newSquares[i] = xIsNext ? 'X' : 'O';
-    // setXIsNext(!xIsNext);  //중복으로 할 경우 한번만 처리가 된다.
-    setXIsNext(prev => !prev);
-    setSquares(newSquares);
-  }
 
   const renderSquare = (i) => {
     return <Square 
@@ -59,8 +13,7 @@ const Board = () => {
 
 
   return (
-    <div>
-      <div className='status'>{status}</div>
+    <div className='board-wrapper'>
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
