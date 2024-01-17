6fbd51af8d6e74c55931d0612660e1275fefe1fb
DS0708 <tlsehdtjd070@naver.com>
2024-01-18 01:24:08 +0900

    REDUX

--- Ch06_Redux/react-redux-app/src/App.tsx
+++ Ch06_Redux/react-redux-app/src/App.tsx
@@ -1,22 +1,59 @@
-import React from 'react';
+import React , {useEffect, useState} from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './reducers/index';
 
 type Props = {
-  value: number;
+  value: any;
   onIncrement: () => void;
   onDecrement: () => void;
-};
+}; 
 
 function App({value, onIncrement, onDecrement} : Props) {
+  const dispatch = useDispatch();
+  const counter = useSelector((state:RootState) => state.counter);
+  const todos : string[] = useSelector((state:RootState) => state.todos);
+
+  const [todoValue, setTodoValue] = useState("");
+
+  const fetchPosts = (): any => {
+    return async function fetchPostsThunk(dispatch:any, getState:any) {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      dispatch({type: "FETCH_POSTS", payload: await response.json()});
+    }
+  }
+
+  useEffect(() => {
+    dispatch(fetchPosts())
+  },[dispatch]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  }
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); //페이지가 refresh되는 것을 강제로 막아줌
+    dispatch({type: 'ADD_TODO', text: todoValue});
+    setTodoValue("");
+  }
+
   return (
     <div className='App'>
-      Clicked: {value} times
+      Clicked: {counter} times
       <button onClick={onIncrement}>
         +
       </button>
       <button onClick={onDecrement}>
         -
       </button>
+
+      <ul>
+        {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange}/>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
--- Ch06_Redux/react-redux-app/src/index.tsx
+++ Ch06_Redux/react-redux-app/src/index.tsx
@@ -3,22 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
-import counter from './reducers/counter';
+import { applyMiddleware ,createStore } from 'redux';
+import rootReducer from './reducers/index';
+import { Provider } from'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const store = createStore(counter);
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("store",store);
+  console.log('action',action);
+  next(action); 
+}
+
+const middleware = applyMiddleware(loggerMiddleware);
+
+const store = createStore(rootReducer);
+
+//todos reducer 확인
+// store.dispatch({type: 'ADD_TODO', text: 'todo1'});
+// console.log('store.getState',store.getState());
 
 const render = () => root.render(
   <React.StrictMode>
-    <App 
-      value={store.getState()}
-      onIncrement={() => store.dispatch({type: 'INCREMENT'})}
-      onDecrement={() => store.dispatch({type: 'DECREMENT'})}
-    /> 
+    <Provider store={store}>
+      <App 
+        value={store.getState()}
+        onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+        onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+      /> 
+    </Provider>
   </React.StrictMode>
 );
 render();
